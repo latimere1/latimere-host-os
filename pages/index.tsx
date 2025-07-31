a02ffd89414c2230f8fdedcef9097c8309694748
Taylor Frost <taylorfrost@Mac.home>
2025-07-31 16:57:41 -0600

    feat: add simple Home page

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Latimere Host OS</h1>
+      <p>Welcome! Use the nav or go to <a href="/properties">/properties</a>.</p>
+    </main>
+  );
+}
+
